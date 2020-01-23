f0a4f611078b4cb7750ca79c1128aa0889be48aa
philyang07 <philyang07@gmail.com>
2020-01-24 00:29:08 +0800

    static

--- gotcha/settings.py
+++ gotcha/settings.py
@@ -130,6 +130,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+AWS_DEFAULT_ACL = None
+
 
 EMAIL_BACKEND="sendgrid_backend.SendgridBackend"
 if os.environ.get('DEBUG'):
@@ -157,6 +164,7 @@ CKEDITOR_CONFIGS = {
         ),
     },
 }
+
 
 # Celery settings
 # CELERY_BROKER_URL = 'pyamqp://guest@localhost//'
